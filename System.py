1c9f2651b4a55ad5683f20c6731d7ac842b70d98
Nitin Anantham <122308011+Nitin-Anantham@users.noreply.github.com>
2023-07-02 19:46:59 +0530

    3

--- System.py
+++ System.py
@@ -62,7 +62,7 @@ if (selected == 'Grey Scale Image'):
             st.success("Processing Completed")
      
         if st.button("Download Sketch Images"):
-            im_pil = Image.fromarray(grayscale_image)  #Displaying Image From Numpy Array
+            #im_pil = Image.fromarray(grayscale_image)  #Displaying Image From Numpy Array
             im_pil.save('final_image.jpeg')
             st.write('Download completed')
            # st.download_button(
